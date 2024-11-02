1609b0a97dbd023c7c56e477ac049e27b9bd319d
Thiago H O S <thiago2006_sales@hotmail.com>
2024-11-02 05:18:02 -0300

    development commit

---
+++ src/main/java/br/com/RestApi/development/RestApiDevelopApplication.java
@@ -0,0 +1,13 @@
+package br.com.RestApi.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/RestApi/development/RestApiDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.RestApi.development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
