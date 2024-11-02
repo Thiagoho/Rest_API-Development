7ab324890a27de762aa53540291a96acf3c13449
Thiago H O S <thiago2006_sales@hotmail.com>
2024-11-02 05:36:03 -0300

    Solicitando uma REquisição no Postman

---
+++ src/main/java/br/com/RestApi/development/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package br.com.RestApi.development.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping
+	public String hello() {
+		return "Oi Thiago";
+	}
+}
